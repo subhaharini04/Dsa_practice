ccf4107f43bc96f6cc34292f95eb7d8307456777
subhaharini04 <subhaharini.act@gmail.com>
2025-06-30 21:56:57 +0530

    linkedlist insertion

--- LinkedList.java
+++ LinkedList.java
@@ -1,27 +1,56 @@
 class Node{
     int num;
     Node next;
-
     public Node(int num) {
         this.num = num;
+    }
+
+    public Node(int num, Node next) {
+        this.num = num;
+        this.next = next;
     }
 }
 public class LinkedList {
     Node head;
     Node tail;
+    static int size=0;
 
-    public void insert(int num){
-        Node node=new Node(num);
+
+    public void insertFirst(int num){
+         Node newNode=new Node(num);
+         newNode.next=head;
+         head=newNode;
+         size++;
+    }
+public void insertEnd(int num){
+        Node newNode=new Node(num);
         if(head==null){
-            head = node;
+            insertFirst(num);
             return;
         }
-        Node current=head;
-        while (current.next!=null){
-            current=current.next;
+            tail.next = newNode;
+            tail = newNode;
+            size++;
+}
+
+public void insertMiddle(int num,int target){
+        if(target == 0){
+            insertFirst(num);
+            return;
         }
-        current.next=node;
+        if(size == target){
+            insertEnd(num);
+            return;
+        }
+        Node temp=head.next;
+        for (int i=1;i<target;i++){
+           temp=temp.next;
+        }
+        Node newNode=new Node(num,temp.next);
+        temp.next=newNode;
+    size++;
     }
+
     public void display(){
         if(head==null){
             System.out.println("List Empty");
--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList list=new LinkedList();
-        list.insert(1);
-        list.insert(2);
-        list.insert(3);
-        list.insert(4);
-        list.insert(5);
-        list.insert(6);
+        list.insertEnd(1);
+        list.insertEnd(2);
+        list.insertEnd(3);
+        list.insertEnd(4);
+        list.insertEnd(5);
+        list.insertEnd(6);
+        list.display();
+        list.insertFirst(9);
+        list.display();
+        list.insertMiddle(15,3);
         list.display();
     }
 }
