b460ffe39045462a4cd8332706b49a7d9a510296
subhaharini04 <subhaharini.act@gmail.com>
2025-07-11 20:43:51 +0530

    linkedlist insertion delteion search

--- LinkedList.java
+++ LinkedList.java
@@ -1,6 +1,7 @@
-class Node{
+class Node {
     int num;
     Node next;
+
     public Node(int num) {
         this.num = num;
     }
@@ -10,56 +11,130 @@ class Node{
         this.next = next;
     }
 }
+
 public class LinkedList {
     Node head;
     Node tail;
-    static int size=0;
+    static int size = 0;
 
-
-    public void insertFirst(int num){
-         Node newNode=new Node(num);
-         newNode.next=head;
-         head=newNode;
-         size++;
+    //INSERTION OF NODE
+    public void insertFirst(int element) {
+        Node newNode = new Node(element);
+        newNode.next = head;
+        head = newNode;
+        size++;
     }
-public void insertEnd(int num){
-        Node newNode=new Node(num);
-        if(head==null){
-            insertFirst(num);
-            return;
-        }
-            tail.next = newNode;
-            tail = newNode;
-            size++;
-}
 
-public void insertMiddle(int num,int target){
-        if(target == 0){
-            insertFirst(num);
+    public void insertEnd(int element) {
+        Node newNode = new Node(element);
+        if (head == null) {
+            insertFirst(element);
             return;
         }
-        if(size == target){
-            insertEnd(num);
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        size++;
+    }
+
+    public void insertMiddle(int element, int target) {
+        if (target == 0) {
+            insertFirst(element);
             return;
         }
-        Node temp=head.next;
-        for (int i=1;i<target;i++){
+        if(target>=size){
+            insertEnd(element);
+            return;
+        }
+        Node newNode = new Node(element);
+        Node temp = head;
+        for (int i = 0; i < target - 1; i++) {
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        size++;
+    }
+
+    //DELETION
+    public void deleteFirst() {
+        head = head.next;
+        if(head==null){
+            tail=null;
+        }
+        size--;
+    }
+
+    public void deleteEnd() {
+        if (head == null) {
+            System.out.println("List is already empty.");
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            tail = null;
+            size--;
+            return;
+        }
+       Node temp=head;
+       while(temp.next.next!=null){
            temp=temp.next;
-        }
-        Node newNode=new Node(num,temp.next);
-        temp.next=newNode;
-    size++;
+       }
+       temp.next=null;
+       tail=temp;
+       size--;
     }
 
-    public void display(){
-        if(head==null){
-            System.out.println("List Empty");
+    public void deleteMiddle(int target){
+        if (target < 0 || target >= size) {
+            System.out.println("Invalid index.");
+            return;
+        }
+
+        if (target == 0) {
+            deleteFirst();
+            return;
         }
         Node temp=head;
-        while (temp!=null){
-            System.out.print(temp.num+"->");
+        for(int i=0;i<target-1;i++){
             temp=temp.next;
         }
+        temp.next=temp.next.next;
+        size--;
+    }
+
+    //SEARCH
+
+    public int search(int element){
+        Node temp=head;
+        int i=0;
+        while(temp!=null) {
+            if (temp.num == element) {
+                return i;
+            } else {
+                temp = temp.next;
+                i++;
+            }
+        }
+        return 0;
+    }
+
+    public void reverse(){
+
+    }
+
+    public void display() {
+        if (head == null) {
+            System.out.println("List Empty");
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.num + "->");
+            temp = temp.next;
+        }
         System.out.println("End");
+        System.out.println("Current Size:" + size);
     }
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         LinkedList list=new LinkedList();
-        list.insertEnd(1);
+        list.insertFirst(9);
+        list.display();
         list.insertEnd(2);
         list.insertEnd(3);
         list.insertEnd(4);
         list.insertEnd(5);
         list.insertEnd(6);
         list.display();
-        list.insertFirst(9);
-        list.display();
         list.insertMiddle(15,3);
         list.display();
+        list.deleteMiddle(4);
+        list.display();
+        System.out.println("position of element : "+ list.search(15));
     }
 }
