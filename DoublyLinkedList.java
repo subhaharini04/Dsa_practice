520c7cf1d3a73059bfffb920d5f03efd08f49aca
subhaharini04 <subhaharini.act@gmail.com>
2025-07-17 10:21:49 +0530

    completed dll

---
+++ DoublyLinkedList.java
@@ -0,0 +1,157 @@
+class Node1 {
+    int data;
+    Node1 next;
+    Node1 prev;
+
+    public Node1(int data) {
+        this.data = data;
+    }
+
+    public Node1(int data, Node1 next, Node1 prev) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+public class DoublyLinkedList {
+    Node1 head;
+    Node1 tail;
+    static int size = 0;
+
+    public void insertFirst(int data) {
+        Node1 node = new Node1(data);
+        if (head == null) {
+            head = node;
+            size++;
+            return;
+        }
+        node.next = head;
+        head.prev = node;
+        head = node;
+        size++;
+    }
+
+    public void insertEnd(int data) {
+        Node1 node = new Node1(data);
+        if (head == null) {
+            insertFirst(data);
+            return;
+        }
+        Node1 temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+        node.prev = temp;
+        tail = node;
+        size++;
+    }
+
+    public void insertIndex(int data,int index){
+        Node1 node=new Node1(data);
+        if(index<0 || index>size){
+            System.out.println("Enter the correct Index");
+            return;
+        }
+        if(index==0){
+            insertFirst(data);
+            return;
+        }
+        if(index==size){
+            insertEnd(data);
+            return;
+        }
+        Node1 temp=head;
+        for(int i=0;i<index-1;i++){
+            temp=temp.next;
+        }
+        node.next=temp.next;
+        temp.next.prev=node;
+        temp.next=node;
+        node.prev=temp;
+        size++;
+    }
+
+    public void deleteFirst(){
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        head=head.next;
+        if (head != null) {
+            head.prev = null;
+        }else {
+            tail=null;
+        }
+        size--;
+    }
+
+    public void deleteLast(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        if (head.next == null) {
+            head = null;
+            tail = null;
+            size--;
+            return;
+        }
+        Node1 temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        tail=tail.prev;
+        tail.next=null;
+        size--;
+    }
+
+    public void deleteIndex(int index){
+        if (index < 0 || index >= size) {
+            System.out.println("Invalid index");
+            return;
+        }
+        if(index==0){
+            deleteFirst();
+            return;
+        }
+        if(index==size-1){
+          deleteLast();
+          return;
+        }
+        Node1 temp=head;
+        for(int i=0;i<index-1;i++){
+            temp=temp.next;
+        }
+        temp.next=temp.next.next;
+        temp.next.prev=temp;
+        size--;
+    }
+    public void display() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node1 temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("End");
+    }
+
+    public void reverse(){
+        Node1 temp=null;
+        Node1 current=head;
+        while(current!=null){
+            temp=current.prev;
+            current.prev=current.next;
+            current.next=temp;
+            current=current.prev;
+        }
+        if(temp!=null){
+            head=temp.prev;
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -1,18 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        LinkedList list=new LinkedList();
+        DoublyLinkedList list=new DoublyLinkedList();
         list.insertFirst(9);
+        list.insertFirst(3);
         list.display();
-        list.insertEnd(2);
-        list.insertEnd(3);
         list.insertEnd(4);
-        list.insertEnd(5);
         list.insertEnd(6);
         list.display();
-        list.insertMiddle(15,3);
+        list.insertIndex(2,2);
+        list.insertIndex(8,4);
         list.display();
-        list.deleteMiddle(4);
+        list.deleteFirst();
         list.display();
-        System.out.println("position of element : "+ list.search(15));
+        list.deleteLast();
+        list.display();
+        list.deleteIndex(2);
+        list.display();
+        list.reverse();
+        list.display();
     }
 }
