617712930221c0dab50263bffa1f940d85bce973
subhaharini04 <subhaharini.act@gmail.com>
2025-07-28 20:21:03 +0530

    CircularQueueLinkedlist

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        CircularQueue queue = new CircularQueue(5);
+        QueueCircularLinkedList queue=new QueueCircularLinkedList();
         queue.enqueue(1);
         queue.enqueue(2);
         queue.enqueue(3);
         queue.enqueue(4);
         queue.enqueue(5);
+        queue.display();
+        System.out.println();
         queue.dequeue();
         queue.display();
         System.out.println();
--- QueueCircularLinkedList.java
+++ QueueCircularLinkedList.java
@@ -1,12 +1,51 @@
-class Node4{
+class Node4 {
     int data;
     Node4 next;
-    public Node4(int data){
-        data=this.data;
-        next=null;
+
+    public Node4(int data) {
+        this.data=data ;
+        next = null;
     }
 }
 
 public class QueueCircularLinkedList {
+    Node4 front = null;
+    Node4 rear = null;
 
+    public void enqueue(int num) {
+        Node4 node = new Node4(num);
+        if (rear == null || front == null) {
+            rear = front = node;
+            rear.next = front;
+        } else {
+            rear.next = node;
+            rear = node;
+            rear.next = front;
+        }
+    }
+
+    public void dequeue(){
+        if(rear == null || front == null){
+            System.out.println("Queue is empty");
+            return;
+        }
+        if(front==rear){
+            front=rear=null;
+        }else{
+            front=front.next;
+            rear.next=front;
+        }
+    }
+
+    public void display(){
+        if(rear==null||front==null){
+            System.out.println("Queue is empty");
+            return;
+        }
+        Node4 temp = front;
+        do {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        } while (temp != front);
+    }
 }
